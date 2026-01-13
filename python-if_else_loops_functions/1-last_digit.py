007d5f974f395960d980adb84e3b9041ebcc59f0
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-13 10:31:20 +0000

    Add last digit classification

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
+
+if last_digit > 5:
+    msg = "and is greater than 5"
+elif last_digit == 0:
+    msg = "and is 0"
+else:
+    msg = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {last_digit} {msg}")
