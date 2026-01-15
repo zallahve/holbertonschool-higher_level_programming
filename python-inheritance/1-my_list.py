b0a46cdfc905f5102c7d2bd34343b1961fe8cfdd
zallahve <zallahve@uwaterloo.ca>
2026-01-15 23:34:00 +0000

    Fix MyList print_sorted to not modify list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """Custom list with a method to print sorted contents."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order (does not modify itself)."""
+        """Print the list sorted in ascending order (without modifying it)."""
         print(sorted(self))
