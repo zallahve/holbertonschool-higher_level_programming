31dff56e456f30a162539e2cce3c40c2180227f9
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-16 22:44:22 +0000

    Fix 7-add_item script

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Add all command-line arguments to a list and save it to add_item.json."""
+"""Add all arguments to a list and save it to add_item.json."""
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    """Load list from add_item.json, extend with argv, then save back."""
-    filename = "add_item.json"
-    try:
-        items = load_from_json_file(filename)
-    except Exception:
-        items = []
+filename = "add_item.json"
 
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, filename)
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-
-if __name__ == "__main__":
-    main()
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
