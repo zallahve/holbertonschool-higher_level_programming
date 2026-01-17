e7f8d1ba2b112506566cc8399ff5b1cb658f7147
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-17 23:57:23 +0000

    SQL introduction: utf8mb4 conversion and avg temperatures

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,4 @@
--- Convert database, table and column to utf8mb4 / utf8mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.first_table
-  CONVERT TO CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.first_table
-  MODIFY name VARCHAR(256)
-  COLLATE utf8mb4_unicode_ci;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table`
+  CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
