f1f654e8d9f61dc1d2013b084113b805efca2924
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-17 23:58:16 +0000

    SQL introduction: top cities in July and August

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Top 3 cities average temperature during July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
