647e89c4c48b0dc977c5c98cf68ea53d6252d027
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-17 23:49:04 +0000

    SQL introduction: convert hbtn_0c_0 to utf8mb4

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- Convert database, table and column to utf8mb4 / utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+  CONVERT TO CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+  MODIFY name VARCHAR(256)
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
