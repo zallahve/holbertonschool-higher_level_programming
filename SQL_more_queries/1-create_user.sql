cc905418f9085a31c9fad972cebb3f8c474c63a9
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-18 14:24:01 +0000

    Add SQL_more_queries README

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create user_0d_1 with all privileges (do not fail if already exists)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,8 @@
+# SQL More Queries
+
+MySQL scripts for advanced queries, permissions, constraints, and joins.
+
+## How to run
+```sh
+cat <script.sql> | mysql -hlocalhost -uroot -p
+
