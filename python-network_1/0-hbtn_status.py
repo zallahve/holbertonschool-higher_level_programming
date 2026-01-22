c8af69d482da179dd28e243bdcda2177f4d6c3e6
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-22 08:12:50 +0000

    Add README and urllib status script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         body = response.read()
 
     print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode("utf-8")))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
